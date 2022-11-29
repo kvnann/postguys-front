7a6441026810d6246a3eaea368036d704012fdf0
kvnann <kenanab9@outlook.com>
2022-11-30 00:52:19 +0400

    commitinsallahfinal

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ let enviroment = {}
 
 enviroment.staging = {
     'baseUrl':"http://localhost:3000",
-    'baseUrlBack':"http://postguys-env.eba-xpyhpaw7.us-east-1.elasticbeanstalk.com/"
+    'baseUrlBack':"http://postguys-env.eba-xpyhpaw7.us-east-1.elasticbeanstalk.com"
 }
 
 enviroment.production = {
     'baseUrl':"https://postguys-demo.herokuapp.com",
-    'baseUrlBack':"http://postguys-env.eba-xpyhpaw7.us-east-1.elasticbeanstalk.com/"
+    'baseUrlBack':"http://postguys-env.eba-xpyhpaw7.us-east-1.elasticbeanstalk.com"
 }
 
-export default enviroment.production
+export default enviroment.staging
