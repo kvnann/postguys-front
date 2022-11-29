475b27f41dc50965385a39f68b46c2978d9cdeb0
kvnann <kenanab9@outlook.com>
2022-11-30 01:40:26 +0400

    commitinsallahfinal

--- src/config.js
+++ src/config.js
@@ -6,8 +6,8 @@ enviroment.staging = {
 }
 
 enviroment.production = {
-    'baseUrl':"https://main.d2vq9ezjhsp9ls.amplifyapp.com",
+    'baseUrl':"http://main.d2vq9ezjhsp9ls.amplifyapp.com",
     'baseUrlBack':"https://postguys-env.eba-xpyhpaw7.us-east-1.elasticbeanstalk.com"
 }
 
-export default enviroment.staging
+export default enviroment.production
