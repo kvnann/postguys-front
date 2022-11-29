ca5a313cfdaa70873b076214ecfba76e50d9dd82
kvnann <kenanab9@outlook.com>
2022-11-30 01:29:00 +0400

    commitinsallahfinal

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ let enviroment = {}
 
 enviroment.staging = {
     'baseUrl':"http://localhost:3000",
-    'baseUrlBack':"http://postguys-env.eba-xpyhpaw7.us-east-1.elasticbeanstalk.com"
+    'baseUrlBack':"https://postguys-env.eba-xpyhpaw7.us-east-1.elasticbeanstalk.com"
 }
 
 enviroment.production = {
     'baseUrl':"https://main.d2vq9ezjhsp9ls.amplifyapp.com",
-    'baseUrlBack':"http://postguys-env.eba-xpyhpaw7.us-east-1.elasticbeanstalk.com"
+    'baseUrlBack':"https://postguys-env.eba-xpyhpaw7.us-east-1.elasticbeanstalk.com"
 }
 
 export default enviroment.staging
